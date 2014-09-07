3196a303fa4c592089581ba87b2f2d68d5518bc5
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-09-08 01:13:33 +0200

    Implement ActorContextFacade to track deletage actor behavior

--- com.typesafe.akka.osgi.ds/src/akka/osgi/ds/impl/ActorFacade.scala
+++ com.typesafe.akka.osgi.ds/src/akka/osgi/ds/impl/ActorFacade.scala
@@ -4,18 +4,79 @@ import akka.actor.Props
 import akka.actor.Actor
 import akka.actor.ActorContext
 import akka.actor.ActorCell
+import akka.japi.Procedure
+import scala.concurrent.ExecutionContextExecutor
+import akka.actor.ActorSystem
+import scala.concurrent.duration.Duration
+import akka.actor.ActorRef
 
 class ActorFacade(props: Props, actorBundleContext: ActorBundleContext) extends Actor {
-  
+
+  private var behaviorStack: List[Actor.Receive] = ActorCell.emptyBehaviorStack
+
   val actor = {
-    ActorCell.contextStack.set(context :: Nil)
-    props.newActor() 
+    ActorCell.contextStack.set(new ActorContextFacade(context) :: Nil)
+    val instance = props.newActor()
+    behaviorStack = if (behaviorStack.isEmpty) instance.receive :: behaviorStack else behaviorStack
+    instance
   }
   
+
   val clazz = props.clazz
 
   def receive = actor.receive
-  
-  override def aroundReceive(receive: Actor.Receive, msg: Any): Unit = 
-    actorBundleContext.run(clazz, actor.aroundReceive(actor.receive, msg))
+
+  override def aroundReceive(receive: Actor.Receive, msg: Any): Unit =
+    actorBundleContext.run(clazz, actor.aroundReceive(behaviorStack.head, msg))
+
+  class ActorContextFacade(context: ActorContext) extends ActorContext {
+
+    def become(behavior: Actor.Receive, discardOld: Boolean = true): Unit =
+      behaviorStack = behavior :: (if (discardOld && behaviorStack.nonEmpty) behaviorStack.tail else behaviorStack)
+
+    def unbecome(): Unit = {
+      val original = behaviorStack
+      behaviorStack =
+        if (original.isEmpty || original.tail.isEmpty) actor.receive :: ActorCell.emptyBehaviorStack
+        else original.tail
+    }
+
+    def self: ActorRef = context.self
+
+    def props: Props = context.props
+
+    def receiveTimeout: Duration = context.receiveTimeout
+
+    def setReceiveTimeout(timeout: Duration): Unit = context.setReceiveTimeout(timeout)
+
+    def sender(): ActorRef = context.sender()
+
+    def children: scala.collection.immutable.Iterable[ActorRef] = context.children
+
+    def child(name: String): Option[ActorRef] = context.child(name)
+
+    implicit def dispatcher: ExecutionContextExecutor = context.dispatcher
+
+    implicit def system: ActorSystem = context.system
+
+    def parent: ActorRef = context.parent
+
+    def watch(subject: ActorRef): ActorRef = context.watch(subject)
+
+    def unwatch(subject: ActorRef): ActorRef = context.unwatch(subject)
+
+    def actorOf(props: Props): ActorRef = context.actorOf(props)
+
+    def actorOf(props: Props, name: String): ActorRef = context.actorOf(props, name)
+
+    def stop(actor: akka.actor.ActorRef): Unit = context.stop(actor)
+
+    def guardian: akka.actor.InternalActorRef = ???
+
+    def lookupRoot: akka.actor.InternalActorRef = ???
+
+    def provider: akka.actor.ActorRefProvider = ???
+
+    def systemImpl: akka.actor.ActorSystemImpl = ???
+  }
 }
