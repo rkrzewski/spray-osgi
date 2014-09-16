4d1ffab48bef914b6e00303f7e919ada19660e8c
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-09-16 22:51:58 +0200

    scaladocs

--- com.typesafe.akka.osgi.ds/src/akka/osgi/ds/ActorService.scala
+++ com.typesafe.akka.osgi.ds/src/akka/osgi/ds/ActorService.scala
@@ -3,12 +3,35 @@ package akka.osgi.ds
 import akka.actor.ActorRef
 import akka.actor.ScalaActorRef
 
+/**
+ * A marker trait for OSGi service implemented as Akka actors.
+ *
+ * <p>Applications may subclass this trait, and register the instances of subclass into OSGi
+ * framework to advertise their functionality implemented using Akka actors to other components.
+ * This is preferred to publishing an {@code ActorRef} as service directly, because the clients
+ * wouldn't be able to track the specific service using it's class, but the provider and the
+ * clients would need to agree on some additional service properties instead.</p>
+ *
+ * <p>The subclass of {@code ActorService} should be placed in the exported API package of the
+ * component providing the service, along with a companion object defining supported messages.<p>
+ */
 trait ActorService {
+
+  /** Returns the ActorRef for the actor performing the actual service. */
   def apply(): ActorRef
 }
+
+/** 
+ * Companion object for the {@code ActorService} trait providing implicit conversions allowing 
+ * using instances of the actor service with syntax typical for an {@code ActorRef}.  
+ */
 object ActorService {
+
+  /** Allows using {@code !} operator on {@code ActorService} instance. */
   implicit def scalaActorRef(service: ActorService): ScalaActorRef =
     service()
-  implicit def ask(service: ActorService) = 
+
+  /** Allows using {@code ?} operator on {@code ActorService} instance. */
+  implicit def ask(service: ActorService) =
     akka.pattern.ask(service())
 }
--- io.spray.osgi/src/spray/osgi/RouteService.scala
+++ io.spray.osgi/src/spray/osgi/RouteService.scala
@@ -2,6 +2,16 @@ package spray.osgi
 
 import spray.routing.Route
 
+/**
+ * An OSGi service that provides a Spray route that should be included in the servers configuration.
+ *
+ * <p>Spray server Declarative Services component is tracking services of this type and adjusts the
+ * configuration appropriately. {@link org.osgi.framework.Constants.SERVICE_RANKING} service property
+ * values will be used for order of precedence of available routes.</p>
+ */
 trait RouteService {
+
+  /** Returns a route definition */
   def apply(): Route
+
 }
