e99805d57bd158e25beae53af85e0c71d6a5aa9e
Rafał Krzewski <rafal.krzewski@caltha.pl>
2014-10-28 00:48:55 +0100

    ScalaDocs

--- com.typesafe.akka.osgi.ds/src/akka/osgi/ds/impl/ActorSystemComponent.scala
+++ com.typesafe.akka.osgi.ds/src/akka/osgi/ds/impl/ActorSystemComponent.scala
@@ -23,32 +23,52 @@ import akka.osgi.BundleDelegatingClassLoader
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigValueFactory
 
+/**
+ * A Declarative Services component that provides [[ActorSystemServiceFactory]] with
+ * configuration and registers it with the OSGi framework.
+ *
+ * The component uses `REQUIRE` configuration policy and PID `com.typesafe.akka`. It does not
+ * have a method annotated with `@Modified`, which means modification of the configuration
+ * causes deactivation of the component and subsequent activation of another instance that will
+ * be provided with new configuration.
+ */
 @Component(
-  immediate = true,
   configurationPid = "com.typesafe.akka",
   configurationPolicy = ConfigurationPolicy.REQUIRE)
 class ActorSystemComponent {
 
+  /** Service factory instance. */
   var serviceFactory: ActorSystemServiceFactory = _
 
+  /** Registration object for the service factory*/
   var registration: ServiceRegistration[_] = _
 
+  /**
+   * Starts up the component.
+   * 
+   * At activation, an [[ActorSystemServiceFactory]] will be created and registered
+   * with OSGi framework. It will provide `ActorSystem` service objects customized for 
+   * all requesting bundles.
+   * 
+   * @param ctx `BundleContext` of the `com.typesafe.akka.osgi.ds.impl` bundle
+   * @param properties component properties fetched by Declarative Services runtime from
+   * `ConfigurationAdmin`. 
+   */
   @Activate
   def activate(ctx: BundleContext, properties: java.util.Map[String, _]): Unit = {
     serviceFactory = new ActorSystemServiceFactory(ConfigRecovery.fromProperties(properties))
     registration = ctx.registerService(classOf[ActorSystem].getName(), serviceFactory, null)
   }
 
+  /**
+   * Shuts down the component.
+   * 
+   * At deactivation, all provided service instances will be unregistered, and the `ActorSystem`
+   * underlying [[ActorSystemServiceFactory]] will be also shut down.
+   */
   @Deactivate
   def deactivate: Unit = {
     registration.unregister()
     serviceFactory.shutdown()
   }
-
-  def toConfig(map: java.util.Map[String, _]): Config =
-    map.keySet().filter(_.endsWith(".origin")).foldLeft(ConfigFactory.empty) { (config, originKey) ⇒
-      val key = originKey.replaceAll("\\.origin$", "")
-      val originDesc = map.get(originKey).asInstanceOf[String]
-      config.withValue(key, ConfigValueFactory.fromAnyRef(map.get(key), originDesc))
-    }
 }
